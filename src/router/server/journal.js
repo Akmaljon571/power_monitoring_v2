c097e8f86e3ed2eb5b28e93a053c5db329a4caa1
Akobir <salomxayir.sx@gmail.com>
2024-02-16 16:14:43 +0500

    journal

--- src/router/server/journal.js
+++ src/router/server/journal.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const { adminToken_superToken } = require("../../middleware/admin_or_super_token");
-const { getLastInsertedJournal } = require("../../controller/server/journal");
+const { getLastInsertedJournal, getLastSuccessfullyInsertedJournal, getJournalList } = require("../../controller/server/journal");
 
 module.exports.journalRouter = Router()
     .get('/last-inserted/:meter_id', adminToken_superToken, getLastInsertedJournal)
+    .get('/last-successful-inserted/:meter_id', adminToken_superToken, getLastSuccessfullyInsertedJournal)
+    .get('/list/:meter_id', adminToken_superToken, getJournalList)
